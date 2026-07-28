4f66ca9c3fb2cfafeb297adac813a76eead7a3c3
ODIRAA-git <odiraa_perpetua@yahoo.com>
2026-07-28 14:39:43 +0200

    feat: add project routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 
 const app = express();
+app.use("/projects", projectRoutes);
 
 app.use(cors());
 app.use(express.json());
